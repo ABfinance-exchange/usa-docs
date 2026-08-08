8ff951301f0ce4bd15f006b26da5c012a22d09c3
Vicky Han <vicky.han@abfinance.com>
2026-08-08 22:46:58 +0800

    global-api-doc-update: ui update and "get started" refer to Integration Guidance

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
@@ -12,13 +11,15 @@ function HomepageHeader() {
   return (
     <header className={clsx(styles.heroBanner)}>
       <div className="container">
-        <h1 className={styles.heroTitle}>{siteConfig.title}</h1>
+        <h1 className={styles.heroTitle}>
+          ABFinance <span className={styles.heroTitleAccent}>API</span><br />Documentation
+        </h1>
         <p className={styles.heroSubtitle}>Build powerful trading integrations with our APIs</p>
         <div className={styles.buttons}>
           <Link
             className={styles.heroButton}
-            to="/v1/intro">
-            Get Started
+            to="/v1/guide">
+            Get started
           </Link>
         </div>
       </div>
@@ -33,9 +34,6 @@ export default function Home() {
       title={siteConfig.title}
       description="ABFinance API Documentation">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
